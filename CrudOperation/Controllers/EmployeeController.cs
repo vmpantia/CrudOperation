271c5ee98677de95bd65cee237a6cb56756511a1
Vincent Pantia <vincent.m.pantia@hotmail.com>
2022-11-16 15:59:34 +0800

    [u] Initial Commit

---
+++ CrudOperation/Controllers/EmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using CrudOperation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudOperation.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            using(DBModel db = new DBModel())
+            {
+                List<Employee> empList = db.Employees.ToList<Employee>();
+                return Json(new { data = empList }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AddorEdit(int employeeID = 0)
+        {
+            return View(new Employee());
+        }
+
+        [HttpPost]
+        public ActionResult AddorEdit(Employee inputEmployee)
+        {
+            using(DBModel db = new DBModel())
+            {
+                db.Employees.Add(inputEmployee);
+                db.SaveChanges();
+                return Json(new { success=true, message="Saved Successfully" }, JsonRequestBehavior.AllowGet); 
+            }
+        }
+    }
+}
---
+++ CrudOperation/Scripts/Views/Employee.js
@@ -0,0 +1,58 @@
+﻿var Popup, dataTable;
+$(document).ready(function () {
+    dataTable = $('#emplyeeTable').DataTable({
+        "ajax": {
+            "url": "/Employee/GetData",
+            "type": "GET",
+            "dataType": "json"
+        },
+        "columns": [
+            { "data": "Name" },
+            { "data": "Position" },
+            { "data": "Office" },
+            { "data": "Age" },
+            { "data": "Salary" },
+        ],
+        "language": {
+            "emptyTable": "No data found, Please click on <b>Add New</b> Button"
+        }
+    });
+});
+
+function PopupForm(url) {
+    var formDiv = $('<div/>');
+    $.get(url)
+        .done(function (response) {
+            formDiv.html(response);
+
+            Popup = formDiv.dialog({
+                autoOpen: true,
+                resizable: false,
+                title: 'Fill Employee Details',
+                height: 500,
+                width: 700,
+                close: function () {
+                    Popup.dialog('destroy').remove();
+                }
+            });
+        });
+}
+
+function SubmitForm(form) {
+    $.validator.unobtrusive.parse(form);
+    var isValid = $(form).valid()
+    if (isValid) {
+        $.ajax({
+            type: "POST",
+            url: form.action,
+            data: $(form).serialize(),
+            success: function (data) {
+                if (data.success) {
+                    Popup.dialog('close');
+                    dataTable.ajax.reload();
+                }
+            }
+        });
+    }
+    return false;
+}
